5b42d6e5053863b5ee8292edbafc431703f1a8bd
bartosz <badalecki@gmail.com>
2023-08-07 21:58:33 +0200

    rendering templates

--- main.go
+++ main.go
@@ -1,19 +1,63 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, web!!!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+const portNumber = ":8080"
 
-	http.ListenAndServe(":8080", nil)
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.html")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	fmt.Fprintf(w, fmt.Sprintf("About page. Sum is %d", sum))
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	result, err := divideValues(2.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, fmt.Sprintf("Error: %d", err))
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("Result is %f", result))
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
+}
+
+// addValues adds two integers and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by zero")
+		return 0, err
+	}
+	return x / y, nil
+}
+
+// main is the main application function
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
+
+	fmt.Println(fmt.Sprintf("Starting app on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 
 }
